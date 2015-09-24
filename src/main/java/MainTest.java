7fddb92d81ca19df9c54cfaa3ef4519877447d23
baibaichen <baibaichen@gmail.com>
2015-09-24 21:51:35 +0800

    make test run

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,6 +1,4 @@
-/**
- * Created by ChangDev on 9/24/2015.
- */
+import org.antlr.runtime.TokenRewriteStream;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.Context;
 import org.apache.hadoop.hive.ql.parse.ASTNode;
@@ -11,16 +9,18 @@ import org.apache.hadoop.hive.ql.parse.ParseUtils;
 import java.io.IOException;
 
 public class MainTest {
-    public static void main(String args[])
-    {
+    public static void main(String args[]){
         try {
             HiveConf conf = new HiveConf();
             Context ctx = new Context(conf);
-            String query = "";
-            System.out.println("Hello World");
+            String query = "select * from a";
+
             ParseDriver pd = new ParseDriver();
             ASTNode tree = pd.parse(query,ctx);
             tree = ParseUtils.findRootNonNullToken(tree);
+            
+            TokenRewriteStream ts = ctx.getTokenRewriteStream();
+            System.out.println("\nRewritten Query:\n" + ts.toString("xxx"));
         }catch (ParseException ex){
         }catch (IOException ex){
         }
