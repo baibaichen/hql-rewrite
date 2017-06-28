936620833ef8b010f5bb878b0d07a76ee9f24639
Chang chen <baibaichen@gmail.com>
2017-06-28 15:34:27 +0800

    tab 4=>2

--- src/main/java/com/yhd/hive/HQLConvert.java
+++ src/main/java/com/yhd/hive/HQLConvert.java
@@ -19,60 +19,61 @@ import java.util.Map;
 
 public class HQLConvert {
 
-    private static final Logger LOG = LoggerFactory.getLogger(HQLConvert.class);
+  private static final Logger LOG = LoggerFactory.getLogger(HQLConvert.class);
 
-    private  Context context;
-    private Map<String, String> params;
+  private Context context;
+  private Map<String, String> params;
 
-    public HQLConvert(Context context, Map<String, String> params){
-        this.context = context;
-        this.params = params;
+  public HQLConvert(Context context, Map<String, String> params) {
+    this.context = context;
+    this.params = params;
+  }
+
+  public void processReader(BufferedReader r) throws IOException, ParseException {
+    String line;
+    StringBuilder qsb = new StringBuilder();
+
+    while ((line = r.readLine()) != null) {
+      if (!isComment(line)) {
+        qsb.append(line + "\n");
+      }
     }
-    public void processReader(BufferedReader r) throws IOException, ParseException {
-        String line;
-        StringBuilder qsb = new StringBuilder();
+    processLine(StrSubstitutor.replace(qsb.toString(), params, "{$", "}"));
+  }
 
-        while ((line = r.readLine()) != null) {
-            if (! isComment(line)) {
-                qsb.append(line + "\n");
-            }
-        }
-        processLine(StrSubstitutor.replace(qsb.toString(),params,"{$","}"));
+  private boolean isComment(String line) {
+    return line.startsWith("--") || line.startsWith("#");
+  }
+
+  private void processLine(String line) throws ParseException {
+    String command = "";
+    for (String oneCmd : line.split(";")) {
+
+      //code copied from Hive CliDriver.processLine
+      if (StringUtils.endsWith(oneCmd, "\\")) {
+        command += StringUtils.chop(oneCmd) + ";";
+        continue;
+      } else {
+        command += oneCmd;
+      }
+
+      if (StringUtils.isBlank(command)) {
+        continue;
+      }
+      rewrite(oneCmd);
+      command = "";
     }
+  }
 
-    private boolean isComment(String line) {
-        return line.startsWith("--") || line.startsWith("#");
-    }
+  private void rewrite(String oneCmd) throws ParseException {
+    ParseDriver pd = new ParseDriver();
 
-    private void processLine(String line) throws ParseException {
-        String command = "";
-        for (String oneCmd : line.split(";")) {
+    ASTNode tree = pd.parse(oneCmd, context);
+    tree = ParseUtils.findRootNonNullToken(tree);
+    TokenRewriteStream ts = context.getTokenRewriteStream();
 
-            //code copied from Hive CliDriver.processLine
-            if (StringUtils.endsWith(oneCmd, "\\")) {
-                command += StringUtils.chop(oneCmd) + ";";
-                continue;
-            } else {
-                command += oneCmd;
-            }
-
-            if (StringUtils.isBlank(command)) {
-                continue;
-            }
-            rewrite(oneCmd);
-            command = "";
-        }
-    }
-
-    private void rewrite(String oneCmd) throws ParseException {
-        ParseDriver pd = new ParseDriver();
-
-        ASTNode tree = pd.parse(oneCmd,context);
-        tree = ParseUtils.findRootNonNullToken(tree);
-        TokenRewriteStream ts = context.getTokenRewriteStream();
-
-        PrePostOrderTraversor traversor = new PrePostOrderTraversor(new TestVisitor(ts));
-        traversor.traverse(tree);
-        LOG.info("Rewritten Query: {}", ts.toString());
-    }
+    PrePostOrderTraversor traversor = new PrePostOrderTraversor(new TestVisitor(ts));
+    traversor.traverse(tree);
+    LOG.info("Rewritten Query: {}", ts.toString());
+  }
 }
